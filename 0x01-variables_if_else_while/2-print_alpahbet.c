f863eca33ccd8346b997d477bd120c1003236cfd
t-Spirit <ericturkson38@gmail.com>
2022-07-01 14:08:30 -0700

    Alpahbets

---
+++ 0x01-variables_if_else_while/2-print_alpahbet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+/* betty doc */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		print("%c", i);
+	return (0);
+}
