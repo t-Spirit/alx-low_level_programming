5beb0ba2be86a5a35f5cb8560c2c10ba84fa6c7c
t-Spirit <ericturkson38@gmail.com>
2022-06-30 20:49:09 -0700

    Sizeof() function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+int main(void)
+{
+	char achar;
+	int bint;
+	long int clong;
+        long long int dlonglong;
+	float efloat;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(achar)); 
+	printf("Size of an int: %d byte(s)\n", sizeof(binteger)); 
+	printf("Size of a long int: %d byte(s)\n", sizeof(clong)); 
+	printf("Size of a long long int: %d byte(s)\n", sizeof(dlonglong); 
+	printf("Size of a float: %d byte(s)\n", sizeof(efloat));
+	
+	return (0);	
+}
