821b6d26108fae355f98ed83ac01d1b4b78d7776
t-Spirit <ericturkson38@gmail.com>
2022-06-30 21:00:18 -0700

    Sizeof() function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(achar)); 
 	printf("Size of an int: %d byte(s)\n", sizeof(bint)); 
 	printf("Size of a long int: %d byte(s)\n", sizeof(clong)); 
-	printf("Size of a long long int: %d byte(s)\n", sizeof(dlonglong); 
+	printf("Size of a long long int: %d byte(s)\n", sizeof(dlonglong)); 
 	printf("Size of a float: %d byte(s)\n", sizeof(efloat));
 	return (0);	
 }
