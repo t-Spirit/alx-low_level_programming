a5fa5437269bc1fff75ea01a4836612311a981d0
t-Spirit <ericturkson38@gmail.com>
2022-07-01 17:33:28 -0700

    Exempt some alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/** more headers go there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/** betty style doc for function goes there */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z')
+	{
+		if (i == q && i == e)
+		{
+			continue
+		}
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
