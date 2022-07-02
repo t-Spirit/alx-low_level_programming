03403ba8ed238ecd4bc6ec6fb128f9d6a9b8fd7e
t-Spirit <ericturkson38@gmail.com>
2022-07-01 18:36:07 -0700

    Single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes here */
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
