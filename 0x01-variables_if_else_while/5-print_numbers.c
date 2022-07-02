e2abd1c33d4049648d5ff494ff239a062e57d277
t-Spirit <ericturkson38@gmail.com>
2022-07-01 18:01:27 -0700

    List base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers get there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for funciton main goes there */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+}
