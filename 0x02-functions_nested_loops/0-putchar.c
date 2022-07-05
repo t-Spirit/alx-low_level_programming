569a390607b20ce74311eeca5b9610cc81e36db4
t-Spirit <ericturkson38@gmail.com>
2022-07-05 07:14:40 -0700

    Print something

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char a = '_putchar';
+
+	putchar(a);
+	putchar('\n')
+
+	return (0);
+}
