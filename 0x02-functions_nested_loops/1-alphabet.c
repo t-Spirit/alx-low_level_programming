2ccdc6ba7e99a51b10b6519ca46d9596789d7736
t-Spirit <ericturkson38@gmail.com>
2022-07-05 12:59:37 -0700

    Lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* print_alphabet - Print alphabet lowercase.
-*
-* Return: void.
-*/
+ * print_alphabet - Print alphabets in lowercase.
+ *
+ * Return: void
+ */
 
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i <= 122; i++)
+	for (i = 97; i < 123; i++)
 	{
 		_putchar(i);
 	}
