b8bd520094e110934c1d413463de68dc39725a80
t-Spirit <ericturkson38@gmail.com>
2022-07-05 05:22:04 -0700

    Print something

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char a = '_putchar';
+
+	putchar(a);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/* more headers goes there */ 
+
+/**
+ * main - Alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+
+void print_alphabet()
+
+int main(voi)
+{
+	print_alphabet();
+	return (0);
+}
