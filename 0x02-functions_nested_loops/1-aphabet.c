cb8f1a52d4149cfdbc1cc448145015c73ed05b51
t-Spirit <ericturkson38@gmail.com>
2022-07-05 10:11:54 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-aphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/* more headers goes there */
+
+/**
+ * main - Print alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for funciton main goes there */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z', i++)
+	{
+		putchat(i);
+	}
+	putchar('\n');
+}
