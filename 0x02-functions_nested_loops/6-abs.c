864631616648b87cfd12bba2ac6b5c05553dedaf
t-Spirit <ericturkson38@gmail.com>
2022-07-06 04:11:50 -0700

    C funtions with conditions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+nes (15 sloc)  226 Bytes
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
