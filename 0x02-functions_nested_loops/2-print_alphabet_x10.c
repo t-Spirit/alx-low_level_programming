84c4df1ef39d7b7da18ef44625d0a1f9bf3204d3
t-Spirit <ericturkson38@gmail.com>
2022-07-06 03:56:00 -0700

    Print alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,19 @@
 void print_alphabet_x10(void)
 {
 	char ch;
-
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	i = 0;
+
+	while (i < 10)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
 			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
