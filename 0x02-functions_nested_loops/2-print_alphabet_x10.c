e4b63ea0850188e2a8da326b96b03bcf36c7f472
t-Spirit <ericturkson38@gmail.com>
2022-07-06 03:51:19 -0700

    Print alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print lowercase letters 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
